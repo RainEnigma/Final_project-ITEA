217df9881610c569827d7d31a9760d80906c9c88
Sergey Naumchick <sergey.naumchick@gmail.com>
2019-12-24 16:50:17 +0200

    Adding tryes to find one user from several

--- contact_book/main_functional/finding_contact.py
+++ contact_book/main_functional/finding_contact.py
@@ -11,4 +11,13 @@ def finding_obj(word):
                 print()
                 for m_keys, m_value in dict(o_dict_contacts[num_of_row]).items():
                     print(f"{m_keys} - {m_value}")
-    print(list_rows_with_similar)
+    if len(list_rows_with_similar) > 1:
+        clarification_word = input(
+            f"There are {len(list_rows_with_similar)} same contacts, please enter another word:")
+        for num in list_rows_with_similar:
+            print(clarification_word)
+            if clarification_word in dict(o_dict_contacts[num]).values():
+                print()
+                for my_key,my_value in dict(o_dict_contacts[num]).items():
+                    print(f"{my_key} - {my_value}")
+                print()
